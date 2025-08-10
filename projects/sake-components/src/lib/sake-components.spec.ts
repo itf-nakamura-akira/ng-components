6cf430b0fbace503cee2cec0ba7b4084190691a9
Akira Nakamura <nakamura.akira@itfllc.co.jp>
2025-08-10 05:41:08 +0000

    UnitTestの設定更新

--- projects/sake-components/src/lib/sake-components.spec.ts
+++ projects/sake-components/src/lib/sake-components.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SakeComponents } from './sake-components';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 describe('SakeComponents', () => {
     let component: SakeComponents;
@@ -9,6 +10,7 @@ describe('SakeComponents', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [SakeComponents],
+            providers: [provideZonelessChangeDetection()],
         }).compileComponents();
 
         fixture = TestBed.createComponent(SakeComponents);
