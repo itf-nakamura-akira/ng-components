4f460cc6d162f41c42a1f32d92fd602ec5cd572a
Akira Nakamura <nakamura.akira@itfllc.co.jp>
2025-08-10 06:09:25 +0000

    prefix の修正

--- projects/sake-components/src/lib/sake-components.ts
+++ projects/sake-components/src/lib/sake-components.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'lib-sake-components',
+    selector: 'sk-sake-components',
     imports: [],
     template: ` <p>sake-components works!</p> `,
     styles: ``,
